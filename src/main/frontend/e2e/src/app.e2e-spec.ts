44ea2c9da99af2794662d1436ab19e8d83bf87ac
Georgy Surkov <ggsurkov@gmail.com>
2019-05-07 00:24:26 +0300

    to add primeng template

--- src/main/frontend/e2e/src/app.e2e-spec.ts
+++ src/main/frontend/e2e/src/app.e2e-spec.ts
@@ -1,14 +1,10 @@
-import {AppPage} from './app.po';
+import { ParadisePage } from './app.po';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+describe('paradise App', () => {
+  let page: ParadisePage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new ParadisePage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to frontend!');
-  });
 });
--- src/main/frontend/src/app/app.component.spec.ts
+++ src/main/frontend/src/app/app.component.spec.ts
@@ -1,28 +1,30 @@
-import {async, TestBed} from '@angular/core/testing';
-import {AppComponent} from './app.component';
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { AppTopBarComponent } from './app.topbar.component';
+import { AppFooterComponent } from './app.footer.component';
+import { AppMenuComponent, AppSubMenuComponent } from './app.menu.component';
+import { ScrollPanelModule} from 'primeng/primeng';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+      imports: [ RouterTestingModule, ScrollPanelModule ],
+      declarations: [ AppComponent,
+        AppTopBarComponent,
+        AppMenuComponent,
+        AppSubMenuComponent,
+        AppFooterComponent
+      ]
+    });
+    TestBed.compileComponents();
+  });
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'frontend'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('frontend');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to frontend!');
   }));
 });
